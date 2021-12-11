3380fe75a600cd271ce22fcbea243eae89f03781
Łukasz <lukasz.f.d@gmail.com>
2021-12-11 17:47:35 +0100

    First rest api in spring boot

---
+++ restapi/src/test/java/pl/lukaszfd/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lukaszfd.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
