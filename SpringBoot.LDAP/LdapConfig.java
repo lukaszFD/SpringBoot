0722eee66deaa66cfb4317ac448ff010da6ffac4
Łukasz <lukasz.f.d@gmail.com>
2023-12-11 15:23:19 +0100

    update

--- SpringBoot.LDAP/LdapConfig.java
+++ SpringBoot.LDAP/LdapConfig.java
@@ -3,14 +3,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
+import org.springframework.ldap.pool.factory.PoolingContextSource;
+import org.springframework.ldap.support.LdapUtils;
+import org.springframework.ldap.transaction.compensating.manager.ContextSourceTransactionManager;
+import org.springframework.security.ldap.DefaultTlsDirContextAuthenticationStrategy;
 
-import javax.naming.NamingException;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.net.SocketFactory;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Collections;
 
 @Configuration
 public class LdapConfig {
@@ -27,35 +28,51 @@ public class LdapConfig {
     @Value("${ldap.password}")
     private String ldapPassword;
 
-    @Bean
-    public LdapContextSource contextSource() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
-        LdapContextSource contextSource = new LdapContextSource();
-        contextSource.setUrl(ldapUrl);
-        contextSource.setBase(ldapBase);
-        contextSource.setUserDn(ldapUserDn);
-        contextSource.setPassword(ldapPassword);
-        contextSource.setPooled(true);
+    @Value("${ldap.keystore.path}")
+    private String keystorePath;
 
-        // Load keystore
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        try (InputStream keystoreStream = getClass().getClassLoader().getResourceAsStream("keystore.jks")) {
-            keyStore.load(keystoreStream, "your_keystore_password".toCharArray());
-        }
+    @Value("${ldap.keystore.password}")
+    private String keystorePassword;
 
-        // Configure SSL
-        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
-        keyManagerFactory.init(keyStore, "your_key_password".toCharArray());
-
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(keyManagerFactory.getKeyManagers(), null, new SecureRandom());
-
-        contextSource.setSslContext(sslContext);
-
-        return contextSource;
-    }
+    @Value("${ldap.keystore.type}")
+    private String keystoreType;
 
     @Bean
     public LdapTemplate ldapTemplate() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
         return new LdapTemplate(contextSource());
+    }
+
+    @Bean
+    public LdapContextSource contextSource() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException {
+        PoolingContextSource poolingContextSource = new PoolingContextSource();
+        poolingContextSource.setDirContextValidator(ctx -> {});
+        poolingContextSource.setBase(ldapBase);
+        poolingContextSource.setUrl(ldapUrl);
+        poolingContextSource.setUserDn(ldapUserDn);
+        poolingContextSource.setPassword(ldapPassword);
+        poolingContextSource.setAuthenticationStrategy(new DefaultTlsDirContextAuthenticationStrategy());
+
+        // Load keystore
+        KeyStore keyStore = KeyStore.getInstance(keystoreType);
+        try (InputStream keystoreStream = getClass().getClassLoader().getResourceAsStream(keystorePath)) {
+            keyStore.load(keystoreStream, keystorePassword.toCharArray());
+        }
+
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
+
+        TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        trustManagerFactory.init(keyStore);
+
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+
+        poolingContextSource.setSocketFactory(socketFactory(sslContext));
+
+        return poolingContextSource;
+    }
+
+    private SocketFactory socketFactory(SSLContext sslContext) {
+        return sslContext.getSocketFactory();
     }
 }
