937382e4acca664ed1aa38b958c3bda8ac7808a1
Łukasz <lukasz.f.d@gmail.com>
2024-01-31 20:08:07 +0100

    v4

--- SpringBoot.CsvReaderService.MappingStrategy/CsvReaderService.java
+++ SpringBoot.CsvReaderService.MappingStrategy/CsvReaderService.java
@@ -1,11 +1,13 @@
-import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.CsvBindByName;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 import com.opencsv.bean.MappingStrategy;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.exceptions.CsvException;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class CsvReaderService {
@@ -30,7 +32,38 @@ public class CsvReaderService {
             return mappingStrategy;
         } else {
             // Jeśli brak adnotacji, użyj strategii opartej na kolejności
-            return new ColumnPositionMappingStrategy<>();
+            ColumnPositionMappingStrategy<T> mappingStrategy = new ColumnPositionMappingStrategy<>();
+            mappingStrategy.setType(clazz);
+            mappingStrategy.setColumnMapping(createColumnMapping(clazz));
+            return mappingStrategy;
         }
+    }
+
+    private <T> String[] createColumnMapping(Class<T> clazz) {
+        Field[] fields = clazz.getDeclaredFields();
+        int validFieldCount = 0;
+
+        for (Field field : fields) {
+            if (!field.isAnnotationPresent(CsvBindByName.class) || 
+                field.isAnnotationPresent(javax.persistence.GeneratedValue.class)) {
+                continue;  // Pomijamy pola bez adnotacji @CsvBindByName lub generowane automatycznie
+            }
+            validFieldCount++;
+        }
+
+        String[] columnMapping = new String[validFieldCount];
+        int index = 0;
+
+        for (Field field : fields) {
+            if (!field.isAnnotationPresent(CsvBindByName.class) || 
+                field.isAnnotationPresent(javax.persistence.GeneratedValue.class)) {
+                continue;  // Pomijamy pola bez adnotacji @CsvBindByName lub generowane automatycznie
+            }
+
+            CsvBindByName annotation = field.getAnnotation(CsvBindByName.class);
+            columnMapping[index++] = annotation.column();
+        }
+
+        return columnMapping;
     }
 }
