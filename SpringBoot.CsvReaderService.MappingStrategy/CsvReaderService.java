0e378e771359e237d2f2476b5167e2b6a381d138
Łukasz <lukasz.f.d@gmail.com>
2024-02-13 20:06:51 +0100

    update

--- SpringBoot.CsvReaderService.MappingStrategy/CsvReaderService.java
+++ SpringBoot.CsvReaderService.MappingStrategy/CsvReaderService.java
@@ -6,11 +6,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CsvReaderService {
     public <T> List<T> readCsvFile(File file, Class<T> clazz) {
         try (FileReader reader = new FileReader(file)) {
-            MappingStrategy<T> mappingStrategy = createMappingStrategy(clazz);
+            MappingStrategy<T> mappingStrategy = createMappingStrategy(clazz, file);
 
             return new CsvToBeanBuilder<T>(reader)
                     .withMappingStrategy(mappingStrategy)
@@ -23,7 +24,12 @@ public class CsvReaderService {
         return null;
     }
 
-    private <T> MappingStrategy<T> createMappingStrategy(Class<T> clazz) {
+    private <T> MappingStrategy<T> createMappingStrategy(Class<T> clazz, File file) {
+        if (shouldUseRFC4180MappingStrategy(file)) {
+            return new RFC4180MappingStrategy<>(clazz);
+        }
+
+        // Jeśli nie używamy RFC4180MappingStrategy, sprawdź inne strategie
         if (hasCsvBindByNameFields(clazz)) {
             // Użyj strategii opartej na nagłówku, jeśli klasa ma pola z adnotacją @CsvBindByName
             HeaderColumnNameMappingStrategy<T> mappingStrategy = new HeaderColumnNameMappingStrategy<>();
@@ -35,6 +41,31 @@ public class CsvReaderService {
             mappingStrategy.setType(clazz);
             return mappingStrategy;
         }
+    }
+
+    private <T> boolean shouldUseRFC4180MappingStrategy(File file) {
+        try (FileReader fileReader = new FileReader(file)) {
+            char separator = detectSeparator(fileReader);
+            boolean containsQuotes = detectQuotes(fileReader);
+            return separator == ',' && containsQuotes;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // Domyślnie nie używaj RFC4180MappingStrategy w przypadku błędu
+        }
+    }
+
+    private char detectSeparator(FileReader fileReader) throws IOException {
+        char separator = ',';
+        String firstLine = new CSVReaderBuilder(fileReader).readNext()[0]; // Odczytaj pierwszą linię
+        if (Pattern.compile("\\t").matcher(firstLine).find()) {
+            separator = '\t'; // Jeśli w pierwszej linii znajduje się tabulator, ustaw separator na tabulator
+        }
+        return separator;
+    }
+
+    private boolean detectQuotes(FileReader fileReader) throws IOException {
+        String firstLine = new CSVReaderBuilder(fileReader).readNext()[0];
+        return firstLine.contains("\"");
     }
 
     private <T> boolean hasCsvBindByNameFields(Class<T> clazz) {
