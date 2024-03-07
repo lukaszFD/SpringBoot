80a76a8777f0a8adc29382a1ea54d637c3654dc1
Łukasz <lukasz.f.d@gmail.com>
2024-03-07 08:22:25 +0100

    update

--- SpringBoot.XlsxReaderApp/ExcelReader.java
+++ SpringBoot.XlsxReaderApp/ExcelReader.java
@@ -27,12 +27,10 @@ public class ExcelReader {
             Iterator<Row> rowIterator = sheet.iterator();
 
             if (rowIterator.hasNext()) {
-                Row headerRow = rowIterator.next();
-                List<String> headers = readRow(headerRow);
 
                 while (rowIterator.hasNext()) {
                     Row dataRow = rowIterator.next();
-                    T obj = mapRowToClass(dataRow, headers, clazz);
+                    T obj = mapRowToClass(dataRow, clazz);
                     result.add(obj);
                 }
             }
@@ -41,54 +39,28 @@ public class ExcelReader {
         return result;
     }
 
-    private static <T> T mapRowToClass(Row row, List<String> headers, Class<T> clazz) {
+    private static <T> T mapRowToClass(Row row, Class<T> clazz) {
         T obj;
         try {
             obj = clazz.newInstance();
             Iterator<Cell> cellIterator = row.cellIterator();
+			Field[] fields = clazz.getDeclaredFields();
+			int entityFieldIndex = 1;
 
             while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
-                int columnIndex = cell.getColumnIndex();
-                String header = headers.get(columnIndex);
+				Field[] field = fields[entityFieldIndex];
+				field.setAccessible(true);
+				field.setAccessible(true);
+				
+				field.set(obj, cell.toString());
+				field.setAccessible(false);
+				entityFieldIndex++;
 
-                Field[] fields = clazz.getDeclaredFields();
-                for (Field field : fields) {
-                    if (field.isAnnotationPresent(Column.class) &&
-                            field.getAnnotation(Column.class).name().equals(header)) {
-                        field.setAccessible(true);
-
-                        if (field.getType().equals(String.class)) {
-                            field.set(obj, cell.getStringCellValue());
-                        } else if (field.getType().equals(int.class)) {
-                            field.set(obj, (int) cell.getNumericCellValue());
-                        } else if (field.getType().equals(double.class)) {
-                            field.set(obj, cell.getNumericCellValue());
-                        } else if (field.getType().equals(Date.class)) {
-                            field.set(obj, cell.getDateCellValue());
-                        }
-                        // Dodaj obsługę innych typów pól według potrzeb
-
-                        field.setAccessible(false);
-                        break;
-                    }
-                }
             }
         } catch (InstantiationException | IllegalAccessException e) {
-            throw new RuntimeException("Błąd podczas mapowania wiersza na obiekt", e);
+            throw new RuntimeException(e);
         }
         return obj;
-    }
-
-    private static List<String> readRow(Row row) {
-        List<String> headers = new ArrayList<>();
-        Iterator<Cell> cellIterator = row.cellIterator();
-
-        while (cellIterator.hasNext()) {
-            Cell cell = cellIterator.next();
-            headers.add(cell.getStringCellValue());
-        }
-
-        return headers;
     }
 }
