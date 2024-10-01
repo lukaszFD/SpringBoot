5d13765c1a6b81eebadef1eae1b266456141b4c8
Łukasz <lukasz.f.d@gmail.com>
2024-10-01 12:42:35 +0200

    Update

---
+++ xlsxmaping.java
@@ -0,0 +1,33 @@
+private <T> T mapRowToClass(Row row, Class<T> clazz) {
+    try {
+        T obj = clazz.getDeclaredConstructor().newInstance();
+        Field[] fields = clazz.getDeclaredFields();
+        
+        // Przetwarzaj pola klasy
+        for (Field field : fields) {
+            // Sprawdź, czy pole ma adnotację ExcelColumn
+            if (field.isAnnotationPresent(ExcelColumn.class)) {
+                field.setAccessible(true);
+
+                // Pobierz numer kolumny z adnotacji
+                ExcelColumn excelColumn = field.getAnnotation(ExcelColumn.class);
+                int columnIndex = excelColumn.value();
+
+                // Pobierz komórkę na podstawie numeru kolumny
+                Cell cell = row.getCell(columnIndex, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+
+                // Sprawdź, czy komórka jest pusta
+                if (cell == null || cell.getCellType() == CellType.BLANK) {
+                    field.set(obj, null);  // Ustaw null dla pustych komórek
+                } else {
+                    // Zawsze konwertujemy wartość komórki na String
+                    field.set(obj, cell.toString());
+                }
+            }
+        }
+        
+        return obj;
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+        throw new RuntimeException(e);
+    }
+}
